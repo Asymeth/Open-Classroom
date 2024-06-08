e04c13e115fada9a07fe583329fd81d027b32331
Asymeth <166385245+Asymeth@users.noreply.github.com>
2024-06-08 15:41:33 +0200

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,17 @@
+lancerJeu()
+
+let inputTextJoueur = document.getElementById("inputEcriture");
+
+let btnValidation = document.getElementById("btnValiderMot");
+
+let zoneProposition = document.querySelector(".zoneProposition");
+
+let zoneScore = document.querySelector("#zoneScore span");
+
+let btnChoix = document.querySelectorAll(".zoneChoix input");
+
+console.log(btnChoix)
+console.log(zoneScore)
+console.log(zoneScore)
+console.log(btnValidation)
+console.log(inputTextJoueur)
